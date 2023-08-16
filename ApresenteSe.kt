9de5ca432e8fb40a057ed9f6eae44fae124dbbd3
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-16 20:39:34 -0300

    salve

---
+++ ApresenteSe.kt
@@ -0,0 +1,9 @@
+func main() {
+    var nome = "Ricardo Móveis" // string
+    var inicial = "R" // string
+    var capacidade = 5.if // float
+    
+
+    println("A squad se chama $nome, possui $capacidade membros. Inicial $inicial")
+
+}
