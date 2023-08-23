f539f4ea67c2e7e1ed17b0655d47ba11e259f589
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-23 19:41:26 -0300

    Adicionei a pasta e arquivo mediaEscolar

---
+++ mediaEscolar/mediaEscolar.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var nota1: Float = 7f
+    var nota2: Float = 4f
+    var nota3: Float = 9f
+
+    var media: Float = (nota1 + nota2 + nota3) / 3
+
+    println("A média das notas: $media")
+}
